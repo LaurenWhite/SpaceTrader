6ee17e6eb10c30c0477481779313876a9d9536e6
LaurenWhite <lauren5826@gmail.com>
2019-02-14 00:01:59 -0500

    Created necessary user configuration files, implemented player class

---
+++ app/src/main/java/edu/gatech/macpack/spacetrader/views/UserConfigurationActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.macpack.spacetrader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.gatech.macpack.spacetrader.R;
+
+public class UserConfigurationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_configuration);
+    }
+}
