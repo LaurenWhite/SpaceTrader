8296182b0eae331bc7ae6d5e59ab364daf675c20
AnthonyBrookss <tygilmartin@gmail.com>
2019-02-26 17:47:48 -0500

    Created ResourceType Enum file

---
+++ app/src/main/java/edu/gatech/macpack/spacetrader/entity/ResourceType.java
@@ -0,0 +1,29 @@
+package edu.gatech.macpack.spacetrader.entity;
+
+public enum ResourceType {
+    NON_SPECIAL_RESOURCES(0),
+    MINERAL_RICH(1),
+    MINERAL_POOR(2),
+    DESERT(3),
+    LOTS_OF_WATER(4),
+    RICH_SOIL(5),
+    POOR_SOIL(6),
+    RICH_FAUNA(7),
+    LIFELESS(8),
+    WEIRD_MUSHROOMS(9),
+    LOTS_OF_HERBS(10),
+    ARTISTIC(11),
+    WARLIKE(12);
+
+
+
+    public int ResourceNumber;
+
+    ResourceType(int ResourceNumber) {
+        this.ResourceNumber = ResourceNumber;
+    }
+
+    public int getResourceNumber() {
+        return ResourceNumber;
+    }
+}
