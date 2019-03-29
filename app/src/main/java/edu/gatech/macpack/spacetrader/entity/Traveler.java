4e3a8b267670b5a53bf685907738cf586853a0f4
LaurenWhite <lauren5826@gmail.com>
2019-03-29 08:10:31 -0400

    Pushing the rest

--- app/src/main/java/edu/gatech/macpack/spacetrader/entity/Traveler.java
+++ app/src/main/java/edu/gatech/macpack/spacetrader/entity/Traveler.java
@@ -3,6 +3,9 @@ package edu.gatech.macpack.spacetrader.entity;
 import android.widget.Space;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.gatech.macpack.spacetrader.views.MarketActivity;
 
 public class Traveler {
@@ -12,12 +15,19 @@ public class Traveler {
     private int fuelNeeded;
     private Planet newLocation;
     private Planet currentLocation;
+    private List<SolarSystem> solarSystems;
 
     public Traveler(SpaceShip ship, Planet currentLocation, Planet newLocation) {
+        this.ship = ship;
         this.currentLocation = currentLocation;
         this.newLocation = newLocation;
-        distance = calculateDistance(currentLocation.getLocation(), newLocation.getLocation());
+
         fuelNeeded = calculateFuelNeeded();
+    }
+
+    public Traveler(SpaceShip ship) {
+        this.ship = ship;
+        solarSystems = Game.getGameInstance().getSolarSystems();
     }
 
     private int calculateDistance(int[] curCoordinate, int[] newCoordinate) {
@@ -30,14 +40,26 @@ public class Traveler {
         return ship.getMileage() * distance;
     }
 
-    public void travel() throws Exception {
+    private int calculateFuelNeeded(int distance) {
+        return ship.getMileage() * distance;
+    }
 
-        if(ship.getFuel() < fuelNeeded) {
-            throw new Exception("Ship doesn't have enough fuel");
-        }
-
+    public void travel() {
         ship.setLocation(newLocation);
         ship.setFuel(ship.getFuel() - fuelNeeded);
+    }
+
+    public List<SolarSystem> systemsInRange() {
+        List<SolarSystem> inRange = new ArrayList<>();
+
+        for(SolarSystem system : solarSystems) {
+            int distance = calculateDistance(ship.getLocation().getParentSystem().getLocation(),
+                                                system.getLocation());
+            if(ship.getFuel() >= calculateFuelNeeded(distance)) {
+                inRange.add(system);
+            }
+        }
+        return inRange;
     }
 
 }
