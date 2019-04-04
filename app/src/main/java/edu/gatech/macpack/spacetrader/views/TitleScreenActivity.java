cdb318a3c17d83fa604db96ad2db99beb0fd1732
Destiny W-Wilson <dysww3@gatech.edu>
2019-04-03 22:21:41 -0400

    implemented travel screen UI

---
+++ app/src/main/java/edu/gatech/macpack/spacetrader/views/TitleScreenActivity.java
@@ -0,0 +1,14 @@
+package edu.gatech.macpack.spacetrader.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.gatech.macpack.spacetrader.R;
+
+public class TitleScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title_screen);
+    }
+}
