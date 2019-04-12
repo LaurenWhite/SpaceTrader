893bd7ce5ac165118cdc1725bf408cea9267cc60
Benjamin Leverette <leveretteben2k17@gatech.edu>
2019-04-12 13:52:44 -0400

    ben junits

---
+++ app/src/test/java/edu/gatech/macpack/spacetrader/buyFromPlayerUnitTest.java
@@ -0,0 +1,64 @@
+package edu.gatech.macpack.spacetrader;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.gatech.macpack.spacetrader.entity.MarketItem;
+import edu.gatech.macpack.spacetrader.entity.Planet;
+import edu.gatech.macpack.spacetrader.entity.SolarSystem;
+import edu.gatech.macpack.spacetrader.entity.TradeGood;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class buyFromPlayerUnitTest {
+    private Planet testPlanet;
+    private MarketItem purchaseItem;
+    private Map<String, MarketItem> market;
+
+    @Before
+    /*
+     * Set up variables for testing
+     */
+    public void setUp() {
+        testPlanet = new Planet(new SolarSystem());
+    }
+
+    @Test
+    /*
+     * Add a new item to market
+     */
+    public void addNewItem() {
+
+        purchaseItem = new MarketItem(TradeGood.WATER, 5, 50);
+        testPlanet.setMarket(new HashMap<String, MarketItem>());
+
+        testPlanet.buyFromPlayer(purchaseItem);
+
+        Map<String, MarketItem> expected = new HashMap<>();
+        expected.put("WATER", new MarketItem(TradeGood.WATER, 5, 50));
+
+        assertEquals(expected, market);
+    }
+
+    @Test
+    /*
+     * Do not sell any items if requested item is not in market
+     */
+    public void addToExisting() {
+        market = new HashMap<>();
+        MarketItem marketStock = new MarketItem(TradeGood.WATER, 5, 50);
+        market.put("WATER", marketStock);
+
+        purchaseItem = new MarketItem(TradeGood.WATER, 3, 50);
+
+        Map<String, MarketItem> expected = new HashMap<>();
+        expected.put("WATER", new MarketItem(TradeGood.WATER, 8, 50));
+        testPlanet.buyFromPlayer(purchaseItem);
+
+        assertEquals(expected, market);
+    }
+}
